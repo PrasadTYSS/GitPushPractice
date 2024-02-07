fe91b1487567c93214be1b87be377380853faa7c
Prasad Hiremath <Prasad Hiremath@TYSS-Prasad-Hiremath>
2024-02-07 17:15:52 +0530

    First Commit

---
+++ GitHubPractice/src/main/java/genericLibrary/PrasadHiremath.java
@@ -0,0 +1,23 @@
+package genericLibrary;
+
+public class PrasadHiremath 
+{
+	String name = "Prasad Hiremath";
+	int id = 03;
+	String company = "testyantra";
+	
+	
+	public void travel()
+	{
+	System.out.println("Loves To Travel");	
+	}
+	
+	public void sleep()
+	{
+		System.out.println("Loves To sleep");
+	}
+	
+	
+	
+	
+}
